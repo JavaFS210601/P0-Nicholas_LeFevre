e2ecfa0beab747a111cedd699217e7a3a3740c65
Nicholas LeFevre <nlefevre@widener.edu>
2021-06-18 10:51:47 -0400

    Added menu options and a welcome screen

--- MyProject/src/com/revature/Menu.java
+++ MyProject/src/com/revature/Menu.java
@@ -1,8 +1,33 @@
 package com.revature;
 
+import java.util.Scanner;
+
 public class Menu {
 	
+	GymLogin gym = new GymLogin();
 	
+	public void display() {
+		
+		boolean displayMenu = true;
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		System.out.println("*                        *");				   						
+		System.out.println("*  Welcome To Nicks Gym! *");
+		System.out.println("*                        *");
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		
+		
+		
+		while(displayMenu) {
+			
+			System.out.println("-----------------------");
+			System.out.println("Please Choose an Option");
+			System.out.println("-----------------------");
+			
+		}
+	}
 
 
 
