e8695fc2eafa2a1ab648666c8ec8812f11dd7c21
Nicholas LeFevre <nlefevre@widener.edu>
2021-06-18 23:18:48 -0400

    Created the menu I will be using

---
+++ MyProject/src/com/revature/Launcher.java
@@ -0,0 +1,13 @@
+package com.revature;
+
+public class Launcher {
+
+	public static void main(String[] args) {
+		
+		Menu menu = new Menu();
+		
+		menu.display();
+
+	}
+
+}
--- MyProject/src/com/revature/Menu.java
+++ MyProject/src/com/revature/Menu.java
@@ -12,6 +12,7 @@ public class Menu {
 		
 		Scanner scan = new Scanner(System.in);
 		
+		//printing the welcome home screen
 		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~");
 		System.out.println("*                        *");				   						
 		System.out.println("*  Welcome To Nicks Gym! *");
@@ -22,9 +23,43 @@ public class Menu {
 		
 		while(displayMenu) {
 			
+			//First display to tell the user to pick an option
 			System.out.println("-----------------------");
 			System.out.println("Please Choose an Option");
 			System.out.println("-----------------------");
+			
+			
+			//Options for the user to choose
+			//System.out.println("0. Apply for a position?"); If I have time I will add this feature.
+			System.out.println("1. Greetings/Information about my gym!");
+			System.out.println("2. Sign up as a new member");
+			System.out.println("3. Sign in");
+			System.out.println("4. Check the cost of a membership");
+			System.out.println("5. Exit the menu");
+			
+			//integer input since the menu options are numbered and the scan.nextLine() because i'm using an int and not a string.
+			int input = scan.nextInt();
+			scan.nextLine();
+			
+			//switch statement to go through the menu options and be able to sign up for or sign in to the gym.
+			switch(input) {
+			
+			case 1: {
+				System.out.println("Welcome to Nicks gym!");
+				System.out.println("I created this gym in 2020 when the pandemic started.");
+				System.out.println("This gym is for people who have sat inside for the last year and need a little pick me up!");
+				System.out.println("We offer complimentary snacks and I've hired motivational speakers to yell at you when you seem defeated!");
+				System.out.println("My goal is to make everyone feel better about the fact that the world is dying.");
+				System.out.println("So don't get sick, come visit Nick! ");
+				break;
+			}
+			case 2:{
+				
+				break;
+			}
+			
+			}
+			
 			
 		}
 	}
